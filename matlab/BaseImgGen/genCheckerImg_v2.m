518c4218807c2035c12f3d630794ba0760fcc243
Ethan Duwell <eduwell@mcw.edu>
2024-05-03 17:53:40 -0500

    Add files via upload
    Signed-off-by: Ethan Duwell <eduwell@mcw.edu>

--- matlab/BaseImgGen/genCheckerImg_v2.m
+++ matlab/BaseImgGen/genCheckerImg_v2.m
@@ -4,9 +4,8 @@
 chkrBrdSz=25; % checkerboard size (checks.. this times 2 is the number of checks in x and y direction..) 
 sizeArrayOut=5000; % size of array (x and y dimensions in pixels..)
 
-meanLum=127.5; % mean luminance of output checkerboard..
-
-chckrAmplitude=0.01; % checker amplitude (number from 0 to 1.. 1 means amplitde of checker contrast spans 100% the possible range of black to white, 0.5 is 50%, 0 is 0% )
+meanLum=0.3; % desired mean luminance of output checkerboard as fraction of total range 0-255..
+chckrAmplitude=0.1; % checker amplitude (number from 0 to 1.. 1 means amplitde of checker contrast spans 100% the possible range of black to white, 0.5 is 50%, 0 is 0% )
 
 % Output directory location
 outDir="/home/eduwell/SynologyDrive/SNAP/projects/revCorrStim/matlab/BaseImgGen/texImgs"; 
@@ -18,7 +17,6 @@ outName=strcat("checkerBrd_",num2str(sizeArrayOut),"by",num2str(sizeArrayOut),"A
 checkerboard = makeCheckerboard_v2(chkrBrdSz,sizeArrayOut);
 
 %% 2. Adjust the checker amplitude may taking weighted mean of checkerboard and  and a mono-lum array of equal mean lum.
-
 
 % compute mean-lum of checkerboard
 chkrMeanLum=mean(mean(checkerboard));
@@ -36,11 +34,16 @@ chkrMLarray_wAve = (mlarrayWt*double(monoLumArray) + chckrAmplitude*double(check
 
 %% 3. Adjust mean luminance to specified value
 
+chkrMax=max(max(double(checkerboard)));
+chkrMin=min(min(double(checkerboard)));
+meanLumAbs=meanLum*(chkrMax-chkrMin);
+
+
 % compute diff between mean chkrMLarray_wAve lum and desired output mean
 % this will be the required adjustment to add to all pixels to push mean
 % checker lum to desired value
 chkrMLarray_wAve_mean=mean(mean(chkrMLarray_wAve));
-meanAdjust=meanLum-chkrMLarray_wAve_mean;
+meanAdjust=meanLumAbs-chkrMLarray_wAve_mean;
 
 % create a matrix of values all equal to meanAdjust
 adjustMat=ones(size(checkerboard,1),size(checkerboard,2)); % initialize as an array of ones
